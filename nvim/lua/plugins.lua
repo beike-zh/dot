028114ccf06be14754d805cc2eea9417cc02d9a2
beike-zh <aiyoung0924@gmail.com>
2022-02-14 09:58:00 +0800

    cleanup

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -44,50 +44,50 @@ return require('packer').startup(function()
     })
 
     -- Treesitter
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        requires = {"nvim-treesitter/nvim-treesitter-textobjects", "JoosepAlviste/nvim-ts-context-commentstring",
-                    "p00f/nvim-ts-rainbow", "nvim-treesitter/playground"},
-        run = ":TSUpdate",
-        config = function()
-            require('nvim-treesitter.configs').setup({
-                -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-                ensure_installed = "maintained",
+    -- use({
+    --     "nvim-treesitter/nvim-treesitter",
+    --     requires = {"nvim-treesitter/nvim-treesitter-textobjects", "JoosepAlviste/nvim-ts-context-commentstring",
+    --                 "p00f/nvim-ts-rainbow", "nvim-treesitter/playground"},
+    --     run = ":TSUpdate",
+    --     config = function()
+    --         require('nvim-treesitter.configs').setup({
+    --             -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+    --             ensure_installed = "maintained",
 
-                -- Install languages synchronously (only applied to `ensure_installed`)
-                sync_install = false,
+    --             -- Install languages synchronously (only applied to `ensure_installed`)
+    --             sync_install = false,
 
-                -- List of parsers to ignore installing
-                -- ignore_install = {"javascript"},
+    --             -- List of parsers to ignore installing
+    --             -- ignore_install = {"javascript"},
 
-                highlight = {
-                    -- `false` will disable the whole extension
-                    enable = true,
+    --             highlight = {
+    --                 -- `false` will disable the whole extension
+    --                 enable = true,
 
-                    -- list of language that will be disabled
-                    -- disable = {"c", "rust"},
+    --                 -- list of language that will be disabled
+    --                 -- disable = {"c", "rust"},
 
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = false
-                }
-            })
-        end
-    })
+    --                 -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    --                 -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    --                 -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    --                 -- Instead of true it can also be a list of languages
+    --                 additional_vim_regex_highlighting = false
+    --             }
+    --         })
+    --     end
+    -- })
 
-    -- telescope 
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {"nvim-lua/plenary.nvim", "nvim-telescope/telescope-project.nvim", {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            run = "make"
-        }},
-        config = function()
-            require('telescope').setup()
-        end
-    }
+    -- -- telescope 
+    -- use {
+    --     'nvim-telescope/telescope.nvim',
+    --     requires = {"nvim-lua/plenary.nvim", "nvim-telescope/telescope-project.nvim", {
+    --         "nvim-telescope/telescope-fzf-native.nvim",
+    --         run = "make"
+    --     }},
+    --     config = function()
+    --         require('telescope').setup()
+    --     end
+    -- }
 
     use({
         "windwp/nvim-autopairs",
--- zsh/config/zinit.zsh
+++ zsh/config/zinit.zsh
@@ -3,7 +3,8 @@ source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
 
 zinit light mafredri/zsh-async
 zinit light zsh-users/zsh-autosuggestions
-zinit light zsh-users/zsh-syntax-highlighting
+#zinit light zsh-users/zsh-syntax-highlighting
+zinit light zdharma-continuum/fast-syntax-highlighting
 zinit light zsh-users/zsh-history-substring-search
 
 
