1efcab5af162c8105ea489cebae0a625388b5063
beike-zh <aiyoung0924@gmail.com>
2022-08-11 11:19:21 +0800

    clean

--- zsh/config/zinit.zsh
+++ zsh/config/zinit.zsh
@@ -3,15 +3,11 @@ source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
 
 zinit light mafredri/zsh-async
 zinit light zsh-users/zsh-autosuggestions
-#zinit light zsh-users/zsh-syntax-highlighting
 zinit light zdharma-continuum/fast-syntax-highlighting
 zinit light zsh-users/zsh-history-substring-search
 
 zinit ice depth=1
 zinit light romkatv/powerlevel10k
-
-
-# zinit light skywind3000/z.lua
 
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#808080"
 
